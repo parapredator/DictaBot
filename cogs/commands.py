3c50da4441a347c515f46a62a179c8e75d636340
kiki-af <antoresf1@gmail.com>
2022-10-06 13:04:17 +0700

    update commands

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+from discord import Member
 from discord.ext.commands import Cog
 from discord.ext.commands import command
 from simple_chalk import chalk, green
@@ -11,11 +14,15 @@ class Commands(Cog):
     async def ping_command(self, ctx):
         await ctx.send(f"Pong! {round(self.bot.latency * 1000)}ms")
 
+    @command(name="slap", aliases=["hit"])
+    async def slap_member(self, ctx, member: Member, *, reason: Optional[str] = "for no reason"):
+        await ctx.send(f"{ctx.author.display_name} slapped {member.mention} {reason}!")
+
     @Cog.listener()
     async def on_ready(self):
         if not self.bot.ready:
-            self.bot.cogs_ready.ready_up("Commands")
-            print(f'{green.yellow("[SYS]")} Ready to serve you Master')
+            self.bot.cogs_ready.ready_up("commands")
+        print(f'{green.yellow("[SYS]")} Ready to serve you Master')
 
 
 async def setup(bot):
